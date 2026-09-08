d139c2b2ea455faa7968944b8e072c7849117e31
Christian Neubauer <christian.neubauer@ionos.com>
2026-09-08 13:38:10 +0200

    Fix RabbitMQ reconnect

--- src/Common/WaaS.Common.Workflow/RabbitMq/RabbitMqPublisher.cs
+++ src/Common/WaaS.Common.Workflow/RabbitMq/RabbitMqPublisher.cs
@@ -11,20 +11,12 @@ public interface IRabbitMqPublisher
 public sealed class RabbitMqPublisher(IOptions<RabbitMqOptions> options) : IRabbitMqPublisher
 {
     private readonly RabbitMqOptions _options = options.Value;
+    private IConnection? _connection;
+    private IChannel? _channel;
 
     public async Task Publish(string routingKey, byte[] body, string correlationId, CancellationToken cancellationToken = default)
     {
-        var connectionFactory = new ConnectionFactory
-        {
-            HostName = _options.Hostname,
-            Port = _options.Port,
-            UserName = _options.Username,
-            Password = _options.Password,
-            VirtualHost = _options.VirtualHost,
-        };
-
-        await using var connection = await connectionFactory.CreateConnectionAsync(cancellationToken);
-        await using var channel = await connection.CreateChannelAsync(cancellationToken: cancellationToken);
+        var (_, channel) = await OpenConnection(cancellationToken);
 
         await channel.BasicPublishAsync(
             _options.Exchange,
@@ -39,5 +31,28 @@ public sealed class RabbitMqPublisher(IOptions<RabbitMqOptions> options) : IRabb
             body,
             cancellationToken
         );
+    }
+
+    private async Task<(IConnection, IChannel)> OpenConnection(CancellationToken cancellationToken = default)
+    {
+        if (_connection is null || !_connection.IsOpen)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = _options.Hostname,
+                Port = _options.Port,
+                UserName = _options.Username,
+                Password = _options.Password,
+                VirtualHost = _options.VirtualHost,
+            };
+
+            _connection = await connectionFactory.CreateConnectionAsync(cancellationToken);
+            _channel = await _connection.CreateChannelAsync(cancellationToken: cancellationToken);
+        }
+
+        if (_connection is null || _channel is null)
+            throw new InvalidOperationException("Failed to open RabbitMQ connection or channel.");
+
+        return (_connection, _channel);
     }
 }
